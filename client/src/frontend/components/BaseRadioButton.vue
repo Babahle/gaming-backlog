2fefe26c4334615e37bea93e013dea5ca2c1b975
Babahle <mokonebabzini@gmail.com>
2025-06-13 11:33:32 +0200

    feat(radioButton): improve animations and formatting for BaseRadioButton component

--- client/src/frontend/components/BaseRadioButton.vue
+++ client/src/frontend/components/BaseRadioButton.vue
@@ -3,40 +3,45 @@ import {motion} from "motion-v"
 import {computed, ComputedRef, ModelRef} from "vue";
 
 const props = defineProps<{
-  name: string,
-  value: string,
+	name: string,
+	value: string,
 }>();
 
 const emit = defineEmits(['update:modelValue']);
 
 const modelValue: ModelRef<string> = defineModel<string>();
 const isChecked: ComputedRef<boolean> = computed((): boolean => {
-  return modelValue.value === props.value.valueOf();
+	return modelValue.value === props.value.valueOf();
 })
 
 function handleChange() {
-  if (modelValue.value !== props.value) {
-    emit('update:modelValue', props.value);
-  }
+	if ( modelValue.value !== props.value ) {
+		emit('update:modelValue', props.value);
+	}
 }
 </script>
 
 <template>
-  <label
-      :for="value"
-  >
-    <input
-        :id="value"
-        :name="name"
-        :value="value"
-        type="radio"
-        :checked="isChecked"
-        @change="handleChange"
-        class="hidden"
-    />
-    <motion.span @click="handleChange" class="ml-2 select-none inline-block px-2 py-2 cursor-pointer rounded-sm border-primary border"
-                 :class="isChecked ?'bg-primary' : 'bg-none' ">{{ value }}</motion.span>
-  </label>
+	<label
+			:for="value"
+	>
+		<input
+				:id="value"
+				:name="name"
+				:value="value"
+				type="radio"
+				:checked="isChecked"
+				@change="handleChange"
+				class="hidden"
+		/>
+		<motion.span @click="handleChange"
+								 :initial="{scale : 1}"
+								 :animate="{scale: isChecked ? 1.05 : 1, backgroundColor: isChecked ? '#EE4266' : 'transparent',}"
+								 :transition="{duration: 0.2, type: 'spring'}"
+								 class="ml-2 select-none inline-block px-2 py-2 cursor-pointer rounded-sm border-primary border"
+								 :class="isChecked ?'bg-primary' : 'bg-none' ">{{ value }}
+		</motion.span>
+	</label>
 </template>
 
 <style scoped>
