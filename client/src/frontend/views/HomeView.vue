8acf6542518d8ad33374027625272b3e96fc41ef
Babahle <mokonebabzini@gmail.com>
2025-08-11 10:44:05 +0200

    feat(ui): enhance HomeView layout and responsiveness

--- client/src/frontend/views/HomeView.vue
+++ client/src/frontend/views/HomeView.vue
@@ -21,10 +21,10 @@ watchEffect(async () => {
   </div>
 
 
-  <div class="flex flex-row items-start h-screen">
-    <SideBar/>
+  <div class="flex flex-col items-center justify-center md:flex-row md:items-start h-screen">
+    <SideBar class="hidden md:block"/>
     <div class="games-container m-4" v-if="games.length > 0">
-      <div class="grid grid-cols-5 gap-6 auto-rows-auto">
+      <div class="grid grid-cols-1 lg:grid-cols-5 gap-6 auto-rows-auto">
         <GameCard v-for="game in games" :game-object="new Game(game.name, game.platform, game.state, game.imageURL)"/>
       </div>
     </div>
