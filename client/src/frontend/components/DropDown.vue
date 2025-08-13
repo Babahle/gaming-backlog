237f7924a30c74e1a83b17ad286e78ff3add6be5
Babahle <mokonebabzini@gmail.com>
2025-08-13 16:27:08 +0200

    feat(dropdown): add DropDown component and integrate with HomeView filter selection

---
+++ client/src/frontend/components/DropDown.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import {ref} from "vue";
+import {motion} from "motion-v";
+
+let isOpen = ref(false);
+
+
+const props = defineProps<{
+  options: {
+    type: Array<any>,
+    required: true,
+  },
+  modelValue: {
+    type: string
+  },
+}>()
+
+
+function selectOption() {
+}
+
+function toggleDropDown() {
+  isOpen.value = !isOpen.value;
+}
+
+function closeDropDown() {
+}
+
+</script>
+
+<template>
+  <button class="bg-cardBackground p-4 rounded-md border border-primary mb-4" @click="toggleDropDown">Select Filter
+  </button>
+
+  <motion.div :initial="{ opacity: 0, scaleY: 0 }"
+              :animate="{ opacity: 1, scaleY: 1  }"
+              v-if="isOpen" class="dropdown-menu bg-cardBackground rounded-md w-32 px-3">
+    <ul class="flex flex-col items-start text-left hover:cursor-pointer">
+      <li class="w-full py-3 hover:bg-background"
+          v-for="option in props.options"><p>{{
+          option
+        }}</p></li>
+    </ul>
+  </motion.div>
+</template>
+
+<style scoped>
+
+</style>
--- client/src/frontend/views/HomeView.vue
+++ client/src/frontend/views/HomeView.vue
@@ -5,12 +5,14 @@ import GameCard from "../components/GameCard.vue";
 import {Game} from "../models/Game";
 import {Constants} from "../constants/Constants";
 import axios from "axios";
+import DropDown from "../components/DropDown.vue";
 
 let games = ref([]);
+let selectedFilter: string = '';
 watchEffect(async () => {
   const response = await axios.get(Constants.API_URL);
   games.value = await response.data;
-});
+})
 </script>
 
 <template>
@@ -23,6 +25,8 @@ watchEffect(async () => {
 
   <div class="flex flex-col items-center justify-center md:flex-row md:items-start h-screen">
     <SideBar class="hidden md:block"/>
+		<DropDown v-model="selectedFilter" :options="['Playing', 'Completed', 'Not Started']" />
+		<p>Selected Filter: {{ selectedFilter }}</p>
     <div class="games-container m-4" v-if="games.length > 0">
       <div class="grid grid-cols-1 lg:grid-cols-5 gap-6 auto-rows-auto">
         <GameCard v-for="game in games" :game-object="new Game(game.name, game.platform, game.state, game.imageURL)"/>
